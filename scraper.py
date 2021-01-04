380109bcb3d14849119b7ea036dc4e6f2d2fb19d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 00:04:26 +0200

    Beautifying the code.

--- scraper.py
+++ scraper.py
@@ -9,8 +9,8 @@ today = date.today()
 dt = datetime.datetime.now()
 now = dt.strftime("%Y-%m-%d-%H:%M")
 
-# Pre-define case names and their hash names.
-case_dictionary = {"Prisma":"Prisma%20Case",
+# Pre-define skin / item names and their hash names.
+item_list = {"Prisma":"Prisma%20Case",
 "Prisma2":"Prisma%202%20Case", "Revolver": "Revolver%20Case","Danger Zone":"Danger%20Zone%20Case",
 "Horizon Case":"Horizon%20Case"}
 
@@ -19,12 +19,10 @@ datastore = {}
 
 # Pull the data from Steam "API".
 def puller():
-    print()
-    print(today, "\n")
     pulled_data = ""
-    for case in case_dictionary:
-        case_data = ""
-        url = (f"http://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name={case_dictionary[case]}&currency=3")
+    for item in item_list:
+        item_data = ""
+        url = (f"http://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name={item_list[item]}&currency=3")
         pull = requests.get(url)
         data = json.loads(pull.content)
         for point in data:
@@ -33,24 +31,17 @@ def puller():
             else:
                 datapoint = f"{point}"
                 datapoint = datapoint.replace("_", " ")
-                case_data += (f"{datapoint}: {data[point]} ")
-        pulled_data += f"\n{case}\n{case_data}"
+                item_data += (f"{datapoint}: {data[point]} ")
+        pulled_data += f"\n{item}\n{item_data}"
     database(pulled_data)
             
 # Store the data.
 def database(pulled_data):
-    pull = ""
-    pull += pulled_data
-    datastore[now] = pull
+    datastore[now] = pulled_data
     pass
 
 # Test database.
 def print_database():
     print(datastore)
-    #for record in datastore:
-    #    print(record," ",datastore[record])
-    #pass
-
 
 puller()
-print_database()
