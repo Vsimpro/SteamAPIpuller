3d37525b410faa37bd851ce0bdec30e5881c233b
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 17:25:57 +0200

    shaving code like gilette

--- scraper.py
+++ scraper.py
@@ -14,98 +14,72 @@ from datetime import date
 # Global variables.
 today = date.today()
 counter = []
+output_gap = 30
 
 # Pre-define skin / item names and their hash names here.
-item_list = {"Chroma 2":"Chroma%202%20Case",
-"Chroma 3":"Chroma%203%20Case",
-"Clutch":"Clutch%20Case",
-"CS20":"CS20%20Case",
-"Danger Zone":"Danger%20Zone%20Case",
-"Falchion":"Falchion%20Case",
-"Fracture":"Fracture%20Case",
-"Gamma ":"Gamma%20%20Case",
-"Gamma 2":"Gamma%202%20Case",
-"Glove":"Glove%20Case",
-"Horizon ":"Horizon%20%20Case",
-"Huntsman":"Huntsman%20Case",
-"Operation Bravo":"Operation%20Bravo%20Case",
-"Operation Breakout Weapon":"Operation%20Breakout%20Weapon%20Case",
-"Operation Broken Fang":"Operation%20Broken%20Fang%20Case",
-"Operation Hydra":"Operation%20Hydra%20Case",
-"Operation Phoenix Weapon":"Operation%20Phoenix%20Weapon%20Case",
-"Operation Vanguard Weapon":"Operation%20Vanguard%20Weapon%20Case",
-"Operation Wildfire":"Operation%20Wildfire%20Case",
-"Prisma":"Prisma%20Case",
-"Prisma 2":"Prisma%202%20Case",
-"Revolver":"Revolver%20Case",
-"Shadow":"Shadow%20Case",
-"Shattered Web":"Shattered%20Web%20Case",
-"Spectrum":"Spectrum%20Case",
-"Spectrum 2":"Spectrum%202%20Case",
-"Winter Offensive Weapon":"Winter%20Offensive%20Weapon%20Case"}
+item_list = {"Chroma 2":"Chroma%202%20Case", "Chroma 3":"Chroma%203%20Case", "Clutch":"Clutch%20Case", "CS20":"CS20%20Case", "Danger Zone":"Danger%20Zone%20Case",
+"Falchion":"Falchion%20Case", "Fracture":"Fracture%20Case", "Gamma":"Gamma%20Case", "Gamma 2":"Gamma%202%20Case", "Glove":"Glove%20Case", "Horizon":"Horizon%20Case", 
+"Huntsman":"Huntsman%20Weapon%20Case", "Operation Bravo":"Operation%20Bravo%20Case", "Operation Breakout Weapon":"Operation%20Breakout%20Weapon%20Case",
+"Operation Broken Fang":"Operation%20Broken%20Fang%20Case","Operation Hydra":"Operation%20Hydra%20Case", "Operation Phoenix Weapon":"Operation%20Phoenix%20Weapon%20Case",
+"Operation Vanguard Weapon":"Operation%20Vanguard%20Weapon%20Case", "Operation Wildfire":"Operation%20Wildfire%20Case", "Prisma":"Prisma%20Case",
+"Prisma 2":"Prisma%202%20Case", "Revolver":"Revolver%20Case", "Shadow":"Shadow%20Case", "Shattered Web":"Shattered%20Web%20Case",
+"Spectrum":"Spectrum%20Case", "Spectrum 2":"Spectrum%202%20Case", "Winter Offensive Weapon":"Winter%20Offensive%20Weapon%20Case"}
 
 # Stores the data with a date and time.
-datastore = {}
+datastore = []
+datapoint = {}
 
 # Call this function as wait(desired time to wait), and format it in seconds.
 def wait(timer):
-    if timer < 61:
-        wait_time = timer
-        #print("Waiting for ", wait_time, " seconds")
-    else: 
-        wait_time = timer / 60
-        #print("Waiting for ", wait_time, " minutes.")
     time.sleep(timer)
-    #print("Continuing.")
 
 # Store the data.
-def database(pulled_data):
-    dt = datetime.datetime.now()
-    now = dt.strftime("%Y-%m-%d-%H:%M")
-    datastore[now] = pulled_data
+def database(data):
+    datastore.append(data)
 
 # Pull the data from Steam "API".
 def puller():
-    pulled_data = ""
     pulled_items = []
     for item in item_list:
         pulled_items.append(item)
         url = (f"http://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name={item_list[item]}&currency=3")
         pull = requests.get(url)
         data = json.loads(pull.content)
-        if data == None:
-            print(f"\nError in: json.loads((requests.get(url)).content), datavalue is: {data}")
-            print(f"Item: {item}")
-            timer = 10
-            wait(timer)
+        timer = 5
+        wait(timer)
         while True:  
           if data == None:
             print(f"Error in: json.loads((requests.get(url)).content), datavalue is: {data}")
             print(f"Item: {item}")
-            timer = 5
             wait(timer)
+          elif data["success"] == False:
+            print(f"COULD NOT PULL {item}, check it's hashname!")
+            break    
           else: 
-            print(f"Pulled {item}")
+            data["Name"] = item
+            dt = datetime.datetime.now()
+            data["Timestamp"] = dt.strftime("%Y-%m-%d-%H:%M")
+            space = " "
+            gap_for_item = output_gap - len(item)
+            space = space * gap_for_item
+            print(f"Data for item {item}:{space}{data}")
+            database(data)
             break
-        item_data = ""
-        for point in data:
-            if point == "success":
-                pass
-            else:
-                datapoint = f"{point}"
-                datapoint = datapoint.replace("_", " ")
-                item_data += (f"{datapoint}: {data[point]} ")
-        pulled_data += f"\n{item} {item_data}"
-    print(f"\nAcquired data for {pulled_items}.")
-    database(pulled_data)
-            
+
 # Test database.
 def print_database():
+    counter = 0
     with open("backlog.txt", "a", encoding="utf-8") as file:
+        timetag = ""
+        file.write(today)
         for record in datastore:
-            backlog = "\n" + record + " " + datastore[record] + "\n"
-            file.write(backlog)
-            #print(datastore)
+            #file.write(record)
+            for key in record:
+                file.write(key)
+                file.write(":")
+                backlog = str(record[key])
+                file.write(backlog)
+                file.write("\n")
     print(f"Data stored to backlog.")
 
 # This console gives the user an ability to interact with the scraper.
@@ -113,7 +87,7 @@ def userconsole():
     print("Console is open! Simply type a command.")
     while True:
         user = input("")
-        commandNotFoundError = "... didn't recognise the command. Try 'help'?\n"
+        commandNotFoundError = "... didn't recognise the command. Try 'help'?"
         commands_list = ["help", "create", "create backlog", "exit", "status", "clear"]
         commands = ""
         for command in commands_list:
@@ -152,14 +126,15 @@ def userconsole():
                 print("\n" * 50)
         else:
             print(commandNotFoundError)
+# Loops the pulling process.
 def loop():
     while True:
         counter.append(1)
         puller()
-        # print(f"Data received for desired items.\nStoring data...")
+        print(f"Data received for desired items.\n")
         timer = 120
         wait(timer)
-
+# Gives pulling process it's own thread, while going into a while loop for userconsole.
 def main():
     mainthread = threading.Thread(target=loop)
     mainthread.start()
