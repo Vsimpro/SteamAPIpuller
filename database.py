4f8d5950ad7e72c44565b14fdefc49bd83e11ed2
Akira Taguchi <akirahattivatti@gmail.com>
2021-01-08 14:43:30 +0200

    fixed database.py

--- database.py
+++ database.py
@@ -5,16 +5,26 @@ import re
 
 def create(items):
     connection = sqlite3.connect('alkonhyllyt.db')
-    connection.execute('''CREATE TABLE IF NOT EXISTS Data (ID INTEGER PRIMARY KEY, timestamp TEXT, name TEXT, lowest_price INT, volume INT, median_price INT);''')
+    connection.execute('''CREATE TABLE IF NOT EXISTS Data (ID INT PRIMARY KEY, lowest_price REAL, volume INT, median_price REAL, name TEXT, timestamp TEXT);''')
     
     records = []
-    for item in items:
+    for item_dictionary in items:
         list_of_attributes = []
-        for value in item:
+        for key in item_dictionary.keys():
+            value = item_dictionary[key]
+            if key=='success':
+                continue
+            elif key=='lowest_price':
+                value = float(value.replace('?','').replace(',','.'))
+            elif key=='volume':
+                value = int(value.replace(',',''))
+            elif key=='median_price':
+                value = float(value.replace(',','.').replace('?',''))
             list_of_attributes.append(value)
+        print(list_of_attributes)
         records.append(list_of_attributes)
 
-    connection.execute('INSERT INTO Data VALUES (?,?,?,?,?);', records)
+    connection.executemany('INSERT INTO Data(lowest_price, volume, median_price, name, timestamp) VALUES (?,?,?,?,?);', records)
     connection.commit()
     connection.close()
 
@@ -23,4 +33,4 @@ def read():
     cursor.execute('''SELECT timestamp, name, lowest_price, volume, median_price FROM Data;''')
     print(cursor.fetchall())
 
-read()
+create([{'success': True, 'lowest_price': '0,09?', 'volume': '11,879', 'median_price': '0,09?', 'Name': 'Chroma 2', 'Timestamp': '2021-01-07-23:49'},{'success': True, 'lowest_price': '0,03?', 'volume': '13,831', 'median_price': '0,03?', 'Name': 'Muna seinalla', 'Timestamp': '2021-05-07-23:49'}])
