210311ee52fa830b24aa3c428b025da053954919
Akira Taguchi <akirahattivatti@gmail.com>
2021-01-12 17:22:28 +0200

    fixed encoding probelm with database euro sign

--- database.py
+++ database.py
@@ -15,11 +15,11 @@ def create(items):
             if key=='success':
                 continue
             elif key=='lowest_price':
-                value = float(value.replace('?','').replace(',','.'))
+                value = float(value.replace('€','').replace(',','.'))
             elif key=='volume':
                 value = int(value.replace(',',''))
             elif key=='median_price':
-                value = float(value.replace(',','.').replace('?',''))
+                value = float(value.replace(',','.').replace('€',''))
             list_of_attributes.append(value)
         print(list_of_attributes)
         records.append(list_of_attributes)
@@ -32,5 +32,3 @@ def read():
     cursor = sqlite3.connect('alkonhyllyt.db').cursor()
     cursor.execute('''SELECT timestamp, name, lowest_price, volume, median_price FROM Data;''')
     print(cursor.fetchall())
-
-create([{'success': True, 'lowest_price': '0,09?', 'volume': '11,879', 'median_price': '0,09?', 'Name': 'Chroma 2', 'Timestamp': '2021-01-07-23:49'},{'success': True, 'lowest_price': '0,03?', 'volume': '13,831', 'median_price': '0,03?', 'Name': 'Muna seinalla', 'Timestamp': '2021-05-07-23:49'}])
