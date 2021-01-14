548a2fcfe255e41ab5bdb29c5fb8c2f4c2dc9ea8
BuildTools <unconfigured@null.spigotmc.org>
2021-01-14 03:28:10 +0200

    Ready to go web!

--- prediction.py
+++ prediction.py
@@ -1,5 +1,9 @@
 import database
+import time
 from database import SQL_Data
+from flask import Flask, render_template
+app = Flask(__name__)
+
 # Basic logic; calculate the 
 #    priceT = 0
 #    priceY = 0
@@ -32,16 +36,38 @@ def appender(timestamp, name):
     x = float(priceY) - float(priceT)
     prediction = priceT + x
     prediction_list[name] = prediction
-    #print(f"{name}, price today: {priceT}, predicition: {prediction}")
 
+def main():
 #Create a list of items and data.
+    for timestamp in read.datadump:  
+        name = timestamp[1]
+        if not name in item_list:
+            item_list.append(name)
+        price_today[name] = timestamp[-1]
+        appender(timestamp, name)
 
-for timestamp in read.datadump:  
-    name = timestamp[1]
-    if not name in item_list:
-        item_list.append(name)
-    price_today[name] = timestamp[-1]
-    appender(timestamp, name)
 
-#for item in prediction_list:
-    #print(f"{item} {prediction_list[item]}")
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/predictions')
+def home_button():
+    main()
+    predictions = ""
+    for i in prediction_list:
+        space = len("Winter Offensive Weapon")
+        spacer = len(i) - space
+        breaker = spacer * " "
+        allPredictions = f"Case: {i} will tomorrow be:{breaker} {prediction_list[i]}€ <br>"
+        predictions += allPredictions
+    return render_template('predictions.html', variable=predictions)
+
+@app.route('/robots.txt/')
+def robots():
+    return "ladies and gentlemen, we got him."
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    main()
